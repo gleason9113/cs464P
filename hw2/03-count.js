465582895a8fc3e7e5bf055e228d0118aa1d7133
frosty <frosty@Ubuntu.myguest.virtualbox.org>
2023-04-15 13:51:51 -0700

    Ex03 complete.

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -1,3 +1,14 @@
-// Add your code here
+document.addEventListener('DOMContentLoaded', function() { 
+    const input = document.getElementById('input-field'); //Get the input field
+    input.addEventListener('input', function(event) { //Changed keydown to input - wasn't working consistently with keydown
+        let targetString = event.target.value; //Get input string
+        if(targetString.length > 0) { //Verify string length - otherwise it highlights spaces
+            let textBody = document.getElementById('text-body'); //Get the text 
+            let regExp = new RegExp(`\\b${targetString}\\b`, 'gi') //Couldn't get this to work with a literal - suspect the variable breaks it
+            let newText = textBody.textContent.replace(regExp, `<mark>$&</mark>`); //Add <mark> tags
+            textBody.innerHTML = newText; //Set the new text
+        }
+    });
+});
 
-input.addEventListener('keydown', handleKeyDown);
+
