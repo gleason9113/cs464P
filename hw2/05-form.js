2a5f8b9fa82891bb6fe23dc9121810fa31b29478
Ariel Gleason <gleas3@pdx.edu>
2023-04-17 14:24:08 -0700

    Form js completed.

--- hw2/05-form.js
+++ hw2/05-form.js
@@ -4,21 +4,21 @@ form.addEventListener('submit', function(event)  {
   event.preventDefault();
   const name = event.target.elements.name.value;
   const email = event.target.elements.email.value;
-  const menu = event.target.elements.menu;
+  const menu = event.target.elements.regMenu.value;
   const courses = Array.from(event.target.elements.courses).filter((checkbox) => checkbox.checked).map((checkbox) => checkbox.value);
   if(courses.length < 1){
     courses = `None selected!`
   }
-  const comments = event.target.elements.comments;
+  const comments = event.target.elements.comments.value;
   if(comments.length < 1){
     comments = `No comments entered.`;
   }
 
-  console.log(`Name: ${name}
-               Email: ${email}
-               Registration Status: ${menu}
-               Courses Taken:  ${courses}
-               Comments: ${comments}
-               `);
+  console.log(`  Name: ${name}
+  Email: ${email}
+  Registration Status: ${menu}
+  Courses Taken:  ${courses}
+  Comments: ${comments}
+  `);
                
 });
