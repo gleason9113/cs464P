bbe547e5b6a7d99ca188e1d84145e41179282a42
frosty <gleas3@pdx.edu>
2023-04-17 19:46:36 -0700

    HW2 complete

--- hw2/03-count.js
+++ hw2/03-count.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
         let targetString = event.target.value; //Get input string
         if(targetString.length > 0) { //Verify string length - otherwise it highlights spaces
             let textBody = document.getElementById('text-body'); //Get the text 
+            //Regex: \b = word boundary, g = global, i = case insensitive
             let regExp = new RegExp(`\\b${targetString}\\b`, 'gi') //Couldn't get this to work with a literal - suspect the variable breaks it
             let newText = textBody.textContent.replace(regExp, `<mark>$&</mark>`); //Add <mark> tags
             textBody.innerHTML = newText; //Set the new text
