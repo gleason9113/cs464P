1a4a851a41f936922dd891221f4ef1a23d52aa12
Ariel Gleason <gleas3@pdx.edu>
2023-04-27 09:54:09 -0700

    Ex1 almost done - fine tuning needed

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -1,4 +1,27 @@
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-fetch(url);
+const flexbox = document.getElementById('cardbox');
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        const imageURL = `https://thronesapi.com/assets/images/`;
+        data.forEach(character => {
+            //Extract needed data from response
+            const name = character.fullName;
+            const title = character.title;
+            const image = imageURL + `${character.image}`;
+            //Create card element
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+            <img src="${image}" alt="${name}">
+            <h2>${name}</h2>
+            <h3>${title}</h3>
+            `
+            flexbox.appendChild(card);
+        })
+    })
+ 
