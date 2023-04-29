b9ca77821e715e8e9d4fee7df76db239c7dd4703
fr0sty <gleason.ariel@gmail.com>
2023-04-29 10:56:09 -0700

    Ex 1 and 2 complete

--- hw3/01-characters.js
+++ hw3/01-characters.js
@@ -4,24 +4,24 @@ const url = 'https://thronesapi.com/api/v2/Characters';
 const flexbox = document.getElementById('cardbox');
 
 fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        const imageURL = `https://thronesapi.com/assets/images/`;
-        data.forEach(character => {
-            //Extract needed data from response
-            const name = character.fullName;
-            const title = character.title;
-            const image = imageURL + `${character.image}`;
-            //Create card element
-            const card = document.createElement('div');
-            card.classList.add('card', 'align-items-center', 'mx-auto');
-            card.innerHTML = `
-            <img src="${image}" alt="${name}" class="card-img-top mb-3">
+  .then((response) => response.json())
+  .then((data) => {
+    // eslint-disable-next-line no-console
+    console.log(data);
+    const imageURL = 'https://thronesapi.com/assets/images/';
+    data.forEach((character) => {
+      // Extract needed data from response
+      const name = character.fullName;
+      const { title } = character;
+      const image = `${imageURL}${character.image}`;
+      // Create card element
+      const card = document.createElement('div');
+      card.classList.add('card', 'align-items-center', 'mx-auto');
+      card.innerHTML = `
+            <img src="${image}" alt="picture of ${name}" class="card-img-top mb-3">
             <p><strong>${name}</strong></h2>
             <p>${title}</p>
-            `
-            flexbox.appendChild(card);
-        })
-    })
- 
+            `;
+      flexbox.appendChild(card);
+    });
+  });
--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-new */
 const backgroundColors = [
   'rgba(54, 162, 235, 0.8)',
   'rgba(255, 206, 86, 0.8)',
@@ -26,48 +27,57 @@ const borderColors = [
   'rgba(78, 52, 199, 1)',
 ];
 
-// url for the Thrones API
-const url = 'https://thronesapi.com/api/v2/Characters';
+// Map to store house names and counts
+const houseLabels = new Map([
+  ['House Targaryen', 0],
+  ['House Tarly', 0],
+  ['House Stark', 0],
+  ['House Baratheon', 0],
+  ['House Lannister', 0],
+  ['House Greyjoy', 0],
+  ['House Clegane', 0],
+  ['House Baelish', 0],
+  ['House Seaworth', 0],
+  ['House Tyrell', 0],
+  ['Other', 0],
+]);
 
-const houseLabels = {
-  "House Targaryen": 0,
-  "House Tarly": 0,
-  "House Stark": 0,
-  "House Baratheon": 0,
-  "House Lannister": 0,
-  "House Greyjoy": 0,
-  "House Clegane": 0,
-  "House Baelish": 0,
-  "House Seaworth": 0,
-  "Unknown": 0,
-  "House Tyrell": 0,
-  "Other" : 0
+// Fuzzy string matching turns out to be fairly complicated.
+// Since no goals or specific method was given, I decided to use a simple yet ugly method.
+// Accuracy is a bit middling, but it works for the most part.
+// For each character, check the house string and match (string contains) to the houseLabels map
+// Return formatted house name
+// If no match, return 'Other'
+function matchHouse(house) {
+  if (house.includes('Targ')) {
+    return 'House Targaryen';
+  } if (house.includes('Tarly')) {
+    return 'House Tarly';
+  } if (house.includes('Stark')) {
+    return 'House Stark';
+  } if (house.includes('Barat')) {
+    return 'House Baratheon';
+  } if (house.includes('nister')) {
+    return 'House Lannister';
+  } if (house.includes('Greyjoy')) {
+    return 'House Greyjoy';
+  } if (house.includes('Clegane')) {
+    return 'House Clegane';
+  } if (house.includes('Baelish')) {
+    return 'House Baelish';
+  } if (house.includes('Seaworth')) {
+    return 'House Seaworth';
+  } if (house.includes('Tyrell')) {
+    return 'House Tyrell';
+  }
+  return 'Other';
 }
 
-let houses = []
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        data.forEach(character => {
-          if(!houses.includes(character.family)) {
-            houses.push(character.family);
-          }
-        })
-        console.log(houses);
-        for(let i = 0; i < houses.length; i++){
-          const toMatch = houses[i];
-          for (let key in houseLabels) {
-            if (key.includes(toMatch)) {
-              houseLabels[key] += 1;
-            }
-          }
-        }
-        console.log(houseLabels);
-      });
+// Render chart - updated to use the data from the map
 const renderChart = () => {
   const donutChart = document.querySelector('.donut-chart');
 
+  // eslint-disable-next-line no-undef
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
@@ -82,7 +92,46 @@ const renderChart = () => {
         },
       ],
     },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Exercise 02 - Charts', // Exercise title - not recognized in WAVE as a heading :(
+        position: 'top',
+        fontSize: 24,
+      },
+      legend: {
+        display: true,
+        position: 'bottom',
+      },
+    },
   });
 };
 
-renderChart();
+// url for the Thrones API
+const url = 'https://thronesapi.com/api/v2/Characters';
+
+const getApiData = async () => {
+  const houses = [];
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    data.forEach((character) => { // Get house info for each character
+      if (!houses.includes(character.family)) {
+        houses.push(character.family);
+      }
+    });
+    // eslint-disable-next-line no-console
+    console.log(houses);
+    houses.forEach((house) => { // Match each characters house to the map if possible and increment
+      houseLabels.set(matchHouse(house), houseLabels.get(matchHouse(house)) + 1);
+    });
+    renderChart();
+  } catch (error) { // Something sad happened
+    // eslint-disable-next-line no-console
+    console.log(`An error occurred while fetching House data: ${error}`);
+  }
+};
+
+// Call the function
+getApiData(url);
