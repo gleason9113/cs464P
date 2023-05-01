2a2b0e21cc8d342e2d27164f5e2a7efc40f143e0
Gleason <gleasoa1@nala.roche.com>
2023-05-01 13:35:34 -0700

    Started search functionality

--- h35/src/components/Search.js
+++ h35/src/components/Search.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 
-function handleSubmit(event) {
-  console.log("Input: " + event.target)
-}
 
 export default function Search() {
+  const [ name, setName ] = useState('');
+  
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Input: " + event.target.value)
+  }
+
   return (
     <div className="container">
       <form>
         <h1>Search</h1>
-        <label for="name">Enter a character name: </label><br></br>
+        <div className="form-group">
+          <label htmlFor="name">Enter a character name: </label><br></br>
+          <input id="name" name="charName" type="text"></input>
+        </div> 
+        
         <button onClick={handleSubmit}>Search!</button>
       </form>
       
